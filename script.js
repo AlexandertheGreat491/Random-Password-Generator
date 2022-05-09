01c7a30250ff535e2b2ca5786ac1221162210684
agvan <agvandyke25@gmail.com>
2022-05-09 13:22:16 -0500

    modified even listener for generateButton & created function generatePassword()

--- script.js
+++ script.js
@@ -6,16 +6,19 @@ var generateBtn = document.querySelector("#generate");
 //Special characters for the function created
 const specialCharacters = "!@#$%^&*()";
 const generateButton = document.getElementById('generateBtn')
+// Add event listener to generate button
 generateButton.addEventListener('click', writePassword)
 
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
-  var passwordText = document.querySelector("#password");
+  var passwordText = document.querySelector('#password');
 
   passwordText.value = password;
 
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+//Prompts that come up after you click generate password
+function generatePassword(){
+    
+}
