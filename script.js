0f9d61b550ba52979eb826a8c45e991a56b66043
agvan <agvandyke25@gmail.com>
2022-05-10 08:58:36 -0500

    added a multi-line comment about function getInput() to explain logic behind using concat() method w/ pw array to enable the passwrod generator to pull upperCaseLetters, lowerCaseLetters, numbers, specialCharacters from pw array

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-// Assignment code here
 var upperCaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",];
 var lowerCaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
@@ -8,7 +7,11 @@ var password = "";
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
-//Defining the getInput() and variables.
+// Defining the getInput() and variables.
+/* In each condition concat pw with
+special characters, upperCaseLetters, lowerCaseLetters,
+& numbers, so that when password is generated pw is the array
+being pulled*/
 function getInput() {
     var length = parseInt(prompt("Enter number of characters desired between 8 and 128"),10);
     if ((length >= 8) && (length <= 128)) {
