d89ad8858dc91bc21f33a6bc6d022e90bccf8f8d
agvan <agvandyke25@gmail.com>
2022-05-10 19:01:23 -0500

    added lines and puncuation for readability

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
+// Arrays to define the characters used to generate passwords.
 var upperCaseLetters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z",];
 var lowerCaseLetters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 var numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
 var specialCharacters = ["!", "@", "#", "$", "%", "&", "=", ">", "<", "?", "[", "]", "{", "}", "~", "_",];
 var pw = [];
 var password = "";
-// Get references to the #generate element
+
+// Get references to the #generate element.
+
 var generateBtn = document.querySelector("#generate");
 
 // Defining the getInput() and variables.
@@ -12,45 +15,55 @@ var generateBtn = document.querySelector("#generate");
 special characters, upperCaseLetters, lowerCaseLetters,
 & numbers, so that when password is generated pw is the array
 being pulled*/
+
 function getInput() {
 // parseInt() parses a string argument and returns characters.
     var length = parseInt(prompt("Enter number of characters desired between 8 and 128"),10);
     if ((length >= 8) && (length <= 128)) {
         var characters = confirm("Would you like to include special characters?");
+
 // Concat pw array with specialCharacters array.
+
         if (characters == true) {
             pw=pw.concat(specialCharacters);
             minimumSpecialCharacters = functionArray.getRandomspecialCharacters();
             minimumCount++;
         }
         var numbers = confirm("Would you like to include numbers?");
+
 // Concat pw array wiht numbers array.
+
       if (numbers == true) {
           pw=pw.concat(numbers);
         minimumNumbers = functionArray.getRandomNumber();
         minimumCount++;
     } 
     var uppercaseletters = confirm("Would you like to include uppercase letters?");
+
 // Concat pw array with upperCaseLetters array.
+
     if (uppercaseletters == true) {
         pw=pw.concat(upperCaseLetters);
       minimumUpperCases = functionArray.getRandomUpper();
       minimumCount++;
     } 
     var lowercaseletters = confirm("Would you like to include lowercase letters?");
+
 // Concat pw array with lowerCaseLetters array.
+
     if (lowercaseletters == true) {
         pw=pw.concat(lowerCaseLetters);
       minimumLowerCases = functionArray.getRandomLower();
       minimumCount++;
     }
-    
     for (let i = 0; i < length ; i++) {
       var randomNumberPicked = Math.floor(Math.random() * pw.length);
      password += pw[randomNumberPicked];
      console.log(password);
 }
+
 // querySelector used to return the first element that mataches #password.
+
 var passwordText = document.querySelector("#password");
 passwordText.value = password;
 } else {
@@ -60,7 +73,8 @@ return password;
 // getInput() ends & return statement specifies that password must be returned.
 }
 
-//Defining empty variables
+//Defining empty variables.
+
 var minimumCount = 0;
 var minimumNumbers = "";
 var minimumLowerCases = "";
@@ -70,10 +84,13 @@ password += minimumNumbers;
 password += minimumLowerCases;
 password += minimumUpperCases;
 password += minimumSpecialCharacters;
+
 // Function that defines the inputs from generatePassword().
+
 function generatePassword() {
     var options = getInput();
   }
+
   var functionArray = {
 // String.fromCharCode method returns a string.
     getRandomNumber: function() {
@@ -92,6 +109,7 @@ function generatePassword() {
   }
 
 // Write password to the #password input.
+
 function writePassword() {
   var passwordCreteria = getInput();
   //console.log(passwordCreteria);
@@ -106,4 +124,5 @@ var passwordText = document.querySelector("#password");
 }
 
 // Event listener for generate button.
+
 generateBtn.addEventListener("click", writePassword);
